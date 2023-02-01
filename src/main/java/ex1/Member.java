9212b98acf8b74798fc5487fb0cea934cfda924c
yujindonut <bbb3631@naver.com>
2023-02-01 16:12:17 +0900

    [ADD] EntityManager를 이용한 CRUD 실습

---
+++ src/main/java/ex1/Member.java
@@ -0,0 +1,31 @@
+package ex1;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+//@Table(name="USER") // 매핑작업
+public class Member {
+  @Id
+  private Long id;
+//  @Column(name="userName") 매핑작업
+  private String name;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
