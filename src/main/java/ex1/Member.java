967e9ace3bd29d26c6c487ba0348f8174e7abf80
yujindonut <bbb3631@naver.com>
2023-02-02 11:48:22 +0900

    [FEAT] 기본키 매핑

--- src/main/java/ex1/Member.java
+++ src/main/java/ex1/Member.java
@@ -5,8 +5,12 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Date;
 @Entity
+@SequenceGenerator(
+    name = "MEMBER_SEQ_GENERATOR",
+    sequenceName = "MEMBER_SEQ", //매핑할 데이터베이스 시퀀스 이름
+    initialValue = 1, allocationSize = 1)
 public class Member {
-  @Id
+  @Id @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
   @Column(name = "name")
   private String username;
