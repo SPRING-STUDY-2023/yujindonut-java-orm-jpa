ad38120817ba86c1058ef31f0db225085508999a
yujindonut <bbb3631@naver.com>
2023-02-03 11:17:20 +0900

    [FEAT] 객체지향적이지 않은 설계 예시

--- src/main/java/ex1/JpaMain.java
+++ src/main/java/ex1/JpaMain.java
@@ -1,52 +1,45 @@
-//package ex1;
-//
-//import javax.persistence.EntityManager;
-//import javax.persistence.EntityManagerFactory;
-//import javax.persistence.EntityTransaction;
-//import javax.persistence.Persistence;
-//
-//public class JpaMain {
-//
-//  public static void main(String[] args) {
-//    // 애플리케이션 로딩 시점에 딱 한번 생성되어 애플리케이션 전체에서 공유됨
-//    EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-//
-//    // Transaction 단위를 실행할때마다 EntityManager를 만들어줌 - 데이터베이스 커넥션 하나를 받아온것
-//    EntityManager em = emf.createEntityManager();
-//    EntityTransaction tx = em.getTransaction();
-//    tx.begin();
-//
-//    try {
-////       생성
-////      Member member = new Member();
-////      member.setId(1L);
-////      member.setName("한유진");
-//
-////      여기까지 비영속 상태
-////      em.persist(member);
-////      영속상태가 됨. (DB에 저장이 되는 시점X)
-//
-////      조회
-////      Member findMember = em.find(Member.class, 1L);
-//
-////      삭제
-////      em.remove(findMember);
-////
-////      수정
-////      findMember.setName("김영한");
-////      실제 데이터베이스에 commit을 하지 않아도 수정이 된다.
-////      변경이 되었는지 transaction하는 시점에서 수정사항이 생기면 JPA가 업데이트 쿼리를 날린다.
-//
-////      ENUM
-//
-//
-//      tx.commit();
-//    } catch (Exception e) {
-//      tx.rollback();
-//    } finally {
-//      em.close();
-//    }
-//
-//    emf.close();
-//  }
-//}
+package ex1;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+  public static void main(String[] args) {
+    // 애플리케이션 로딩 시점에 딱 한번 생성되어 애플리케이션 전체에서 공유됨
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("Mapping");
+
+    // Transaction 단위를 실행할때마다 EntityManager를 만들어줌 - 데이터베이스 커넥션 하나를 받아온것
+    EntityManager em = emf.createEntityManager();
+    EntityTransaction tx = em.getTransaction();
+    tx.begin();
+
+    try {
+
+      Team team = new Team();
+      team.setName("TEAM A");
+      em.persist(team);
+
+      Member member = new Member();
+      member.setUsername("member1");
+      member.setTeamId(team.getId());
+      em.persist(member);
+
+      // 객체지향적이지 않은 설계
+      Member findMember = em.find(Member.class, member.getId());
+      Long findTeamId = findMember.getTeamId();
+      // 연관관계가 없음
+      Team findTeam = em.find(Team.class, team.getId());
+
+      tx.commit();
+    } catch (Exception e) {
+      tx.rollback();
+    } finally {
+      em.close();
+    }
+
+    emf.close();
+  }
+}
--- src/main/java/ex1/Member.java
+++ src/main/java/ex1/Member.java
@@ -1,84 +1,98 @@
-//package ex1;
-//
-//import javax.persistence.*;
-//import java.time.LocalDate;
-//import java.time.LocalDateTime;
-//import java.util.Date;
-//@Entity
-//@SequenceGenerator(
-//    name = "MEMBER_SEQ_GENERATOR",
-//    sequenceName = "MEMBER_SEQ", //매핑할 데이터베이스 시퀀스 이름
-//    initialValue = 1, allocationSize = 1)
-//public class Member {
-//  @Id @GeneratedValue(strategy = GenerationType.AUTO)
-//  private Long id;
-//  @Column(name = "name")
-//  private String username;
-//  private Integer age;
-//  @Enumerated(EnumType.STRING)
-////  @Enumerated(EnumType.ORDINAL)
-////  ORDINAL로 하면 엄청나게 어려운 버그가 생김 (enum이 앞에 추가 될 경우 다른 값인데 0, 0 이 들어가는 경우)
-//  private RoleType roleType;
-//  @Temporal(TemporalType.TIMESTAMP)
-//  private Date createdDate;
-//  @Temporal(TemporalType.TIMESTAMP)
-//  private Date lastModifiedDate;
-//  @Lob
-//  private String description;
-//
-//  public Long getId() {
-//    return id;
-//  }
-//
-//  public void setId(Long id) {
-//    this.id = id;
-//  }
-//
-//  public String getUsername() {
-//    return username;
-//  }
-//
-//  public void setUsername(String username) {
-//    this.username = username;
-//  }
-//
-//  public Integer getAge() {
-//    return age;
-//  }
-//
-//  public void setAge(Integer age) {
-//    this.age = age;
-//  }
-//
-//  public RoleType getRoleType() {
-//    return roleType;
-//  }
-//
-//  public void setRoleType(RoleType roleType) {
-//    this.roleType = roleType;
-//  }
-//
-//  public Date getCreatedDate() {
-//    return createdDate;
-//  }
-//
-//  public void setCreatedDate(Date createdDate) {
-//    this.createdDate = createdDate;
-//  }
-//
-//  public Date getLastModifiedDate() {
-//    return lastModifiedDate;
-//  }
-//
-//  public void setLastModifiedDate(Date lastModifiedDate) {
-//    this.lastModifiedDate = lastModifiedDate;
-//  }
-//
-//  public String getDescription() {
-//    return description;
-//  }
-//
-//  public void setDescription(String description) {
-//    this.description = description;
-//  }
-//}
+package ex1;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+@Entity(name = "USER")
+@SequenceGenerator(
+    name = "MEMBER_SEQ_GENERATOR",
+    sequenceName = "MEMBER_SEQ", //매핑할 데이터베이스 시퀀스 이름
+    initialValue = 1, allocationSize = 1)
+public class Member {
+  @Id @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "MEMBER_ID")
+  private Long id;
+  @Column(name = "USERNAME")
+  private String username;
+
+  @Column(name="TEAM_ID")
+  private Long teamId;
+
+  private Integer age;
+
+  public Long getTeamId() {
+    return teamId;
+  }
+
+  public void setTeamId(Long teamId) {
+    this.teamId = teamId;
+  }
+
+  @Enumerated(EnumType.STRING)
+//  @Enumerated(EnumType.ORDINAL)
+//  ORDINAL로 하면 엄청나게 어려운 버그가 생김 (enum이 앞에 추가 될 경우 다른 값인데 0, 0 이 들어가는 경우)
+  private RoleType roleType;
+  @Temporal(TemporalType.TIMESTAMP)
+  private Date createdDate;
+  @Temporal(TemporalType.TIMESTAMP)
+  private Date lastModifiedDate;
+  @Lob
+  private String description;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public Integer getAge() {
+    return age;
+  }
+
+  public void setAge(Integer age) {
+    this.age = age;
+  }
+
+  public RoleType getRoleType() {
+    return roleType;
+  }
+
+  public void setRoleType(RoleType roleType) {
+    this.roleType = roleType;
+  }
+
+  public Date getCreatedDate() {
+    return createdDate;
+  }
+
+  public void setCreatedDate(Date createdDate) {
+    this.createdDate = createdDate;
+  }
+
+  public Date getLastModifiedDate() {
+    return lastModifiedDate;
+  }
+
+  public void setLastModifiedDate(Date lastModifiedDate) {
+    this.lastModifiedDate = lastModifiedDate;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+}
